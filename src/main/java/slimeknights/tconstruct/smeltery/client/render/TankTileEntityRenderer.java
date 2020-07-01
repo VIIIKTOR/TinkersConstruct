0dc05478c60e939d9d420d2de88b1d271c6dd190
KnightMiner <knightminer4@gmail.com>
2020-07-01 00:45:55 -0500

    Extract tank logic to a common interface

--- src/main/java/slimeknights/tconstruct/smeltery/client/render/TankTileEntityRenderer.java
+++ src/main/java/slimeknights/tconstruct/smeltery/client/render/TankTileEntityRenderer.java
@@ -5,19 +5,21 @@ import lombok.extern.log4j.Log4j2;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.tileentity.TileEntity;
 import slimeknights.tconstruct.common.config.Config;
 import slimeknights.tconstruct.library.client.RenderUtil;
 import slimeknights.tconstruct.library.client.model.tesr.TankModel;
-import slimeknights.tconstruct.smeltery.tileentity.TankTileEntity;
+import slimeknights.tconstruct.smeltery.tileentity.ITankTileEntity;
 
 @Log4j2
-public class TankTileEntityRenderer extends TileEntityRenderer<TankTileEntity> {
+public class TankTileEntityRenderer<T extends TileEntity & ITankTileEntity> extends TileEntityRenderer<T> {
+
   public TankTileEntityRenderer(TileEntityRendererDispatcher rendererDispatcherIn) {
     super(rendererDispatcherIn);
   }
 
   @Override
-  public void render(TankTileEntity tile, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLightIn, int combinedOverlayIn) {
+  public void render(T tile, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLightIn, int combinedOverlayIn) {
     if (Config.CLIENT.tankFluidModel.get()) {
       return;
     }
